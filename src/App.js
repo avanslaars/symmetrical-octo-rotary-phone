e0f4931ffb2ac91dd60b9a720c4bb110fd394e86
Andrew L. Van Slaars <avanslaars@gmail.com>
2016-10-08 19:57:37 -0400

    Just testing deployment

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
     // gun.get('whatever').put({candy: 'llama'}, function(err){ console.log("did we make it here?", err) });
     todoData.path(counter).put(newItem)
     counter++
-    
     this.setState({
       currentInput: ''
     })
